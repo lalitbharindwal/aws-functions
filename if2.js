2a98505066fe073ea5aedfcae62b72858390ec9f
Lalit Bharindwal <lalitbharindwal@gmail.com>
2023-10-21 16:31:27 +0530

    Update

--- if2.js
+++ if2.js
@@ -1,9 +1,11 @@
-    var script = document.createElement('script');
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://lucent-semifreddo-14f0a4.netlify.app/if.js';
+document.body.appendChild(script);
+   
+   var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = 'https://sdk.amazonaws.com/js/aws-sdk-2.1.12.min.js';
     document.body.appendChild(script);
 
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://lucent-semifreddo-14f0a4.netlify.app/if.js';
-    document.body.appendChild(script);
+    
