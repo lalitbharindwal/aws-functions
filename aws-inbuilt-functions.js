7e4db89e1d558f9eee4e92d2ab8597d9162307f7
Lalit Bharindwal <lalitbharindwal@gmail.com>
2023-10-22 16:13:16 +0530

    Update

--- aws-inbuilt-functions.js
+++ aws-inbuilt-functions.js
@@ -1,16 +1,27 @@
-function aws_config(access_key, secret_access_key, region){
+function config(access_key, secret_access_key, region){
     AWS.config.update({
         //region: "us-east-1",
         region: region,
         //endpoint: "http://localhost:8000",
-        //accessKeyId: "AKIAVVALCTCZPOR5QOH2",
         accessKeyId: access_key,
-        //secretAccessKey: "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"
         secretAccessKey: secret_access_key
         });
 }
 
-function put_object(body, bucket,  key){
+function create_bucket(bucket){
+    var params = {
+        Bucket: bucket
+       };
+       s3.createBucket(params, function(err, data) {
+         if (err) {
+            console.log(err, err.stack); // an error occurred
+         }else{
+            console.log(data);           // successful response
+         }
+       });
+}
+
+function insert_object(body, bucket,  key){
     aws_config()
     var s3 = new AWS.S3();
     var params = {
